c31cd1a3bc13a9c711dcb580ac95ab5e1a3dcedd
se7en <gideon.ahiadzi@gmail.com>
2023-06-09 01:06:42 +0000

    refactored test

--- tests/test_icd/test_views.py
+++ tests/test_icd/test_views.py
@@ -1,23 +1,9 @@
-from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
 
-from icd.models import Category
+from tests.test_icd.test_setup import CategoryTestSetup
 
 
-class CategoryListViewTest(APITestCase):
-    def setUp(self) -> None:
-        self.category1 = Category.objects.create(
-            category_code="A01", category_title="Category 1"
-        )
-        self.category2 = Category.objects.create(
-            category_code="B01", category_title="Category 2"
-        )
-        self.category_list_create_url = reverse("category-list-create")
-        self.category_detail_url = reverse(
-            "category-detail", kwargs={"id": self.category1.id}
-        )
-
+class CategoryViewsTest(CategoryTestSetup):
     def test_list_categories(self):
         response = self.client.get(self.category_list_create_url, format="json")
 
