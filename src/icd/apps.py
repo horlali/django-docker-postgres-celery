28109029f538bce0b3e3948c01e905db9ee3401c
se7en <gideon.ahiadzi@gmail.com>
2023-06-10 02:12:36 +0000

    added signals

--- src/icd/apps.py
+++ src/icd/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class IcdConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "icd"
+
+    def ready(self) -> None:
+        import icd.signals  # noqa: F401
