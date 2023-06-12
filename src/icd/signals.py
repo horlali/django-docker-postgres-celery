a99c2374b0db6201eafaa62977bcf2f26772e8f8
Gideon Ahiadzi <60545561+horlali@users.noreply.github.com>
2023-06-12 16:20:30 +0000

    removed extra task

--- src/icd/signals.py
+++ src/icd/signals.py
@@ -1,12 +1,8 @@
-import os
-
-from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from icd.models import CSVFile
 from services.emails.messaging import send_mail
-from services.utils.process_csv import add_category_data_to_db, add_diagnosis_data_to_db
 
 
 @receiver(post_save, sender=CSVFile)
@@ -17,10 +13,3 @@ def file_post_save(sender, instance, created, **kwargs):
             subject="ICD File Upload",
             content="File uploaded successfully",
         )
-
-        file_path = instance.file.path
-        if instance.type == "CATEGORY":
-            add_category_data_to_db(os.path.join(settings.MEDIA_ROOT, file_path))
-
-        if instance.type == "DIAGNOSIS":
-            add_diagnosis_data_to_db(os.path.join(settings.MEDIA_ROOT, file_path))
